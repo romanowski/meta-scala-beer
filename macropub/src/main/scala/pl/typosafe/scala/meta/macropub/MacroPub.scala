63a364d7054f27ef7ef1afe7d2081ec401b6ff50
Krzysztof Romanowski <romanowski.kr@gmail.com>
2015-08-28 15:13:39 +0200

    task2: bar fails nicely

--- macropub/src/main/scala/pl/typosafe/scala/meta/macropub/MacroPub.scala
+++ macropub/src/main/scala/pl/typosafe/scala/meta/macropub/MacroPub.scala
@@ -9,10 +9,38 @@ object MacroPub {
 
   def pubMacro(c: Context)(makeOrder: c.Expr[(Bartender) => Unit]): c.Expr[Seq[Drink]] = {
     import c.universe._
+
+    object Order {
+      def isOrder(func: Tree) = func.symbol match {
+        case ms: MethodSymbolApi =>
+          ms.name.toString == "order"
+      }
+
+      def unapply(t: Tree): Option[(Tree, Tree)] = t match {
+        case Apply(func, List(drink)) if isOrder(func) =>
+          Some((func, drink))
+        case _ => None
+      }
+    }
+
+    val transformer = new Transformer {
+      override def transform(tree: c.universe.Tree): c.universe.Tree = tree match {
+        case Order(func, drink) =>
+          val newDrinkOrder = q"""
+                                  if (Pub.serves.contains($drink)) $drink
+                                  else throw new NoSuchDrinkInMenu($drink)
+                              """
+          c.typecheck(Apply(super.transform(func), List(newDrinkOrder)))
+        case _ => super.transform(tree)
+      }
+    }
+
+    val newOrder = transformer.transform(makeOrder.tree)
+
     c.Expr(
       q"""
          val bt = Pub.askBartender()
-         $makeOrder(bt)
+         $newOrder(bt)
          Pub.finalizeOrder(bt)
        """)
   }
